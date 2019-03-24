42144dc999b358e780961b305ec32cb6cb6dcc32
LIRAN BARUCH <liran.baruch1@live.biu.ac.il>
2019-03-24 18:42:55 +0200

    continue

--- ex1/ComposedMission.cs
+++ ex1/ComposedMission.cs
@@ -8,6 +8,20 @@ namespace Excercise_1
 {
     public class ComposedMission : IMission
     {
-
+        public event EventHandler<double> OnCalculate;
+        
+        public String Name { get; }
+        public String Type { get; }
+        private List<function> toCompute;
+        public ComposedMission(string v)
+        {
+            Name = v;
+            Type = "Composed";
+        }
+        
+        public double Calculate(double value)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ex1/SingleMission.cs
+++ ex1/SingleMission.cs
@@ -8,6 +8,21 @@ namespace Excercise_1
 {
     public class SingleMission : IMission
     {
-
+        public event EventHandler<double> OnCalculate;
+        public String Name { get; }
+        public String Type { get; }
+        private function function;
+        public SingleMission(function function, string str)
+        {
+            this.function = function;
+            Name = str;
+            Type = "Single";
+        }      
+        public double Calculate(double value)
+        {
+            double result = function(value);
+            OnCalculate?.Invoke(this, result);
+            return result;
+        }
     }
 }
