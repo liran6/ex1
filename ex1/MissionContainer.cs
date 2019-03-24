9ae9dfc383301519f05e1e9009d58ac2f5573515
LIRAN BARUCH <liran.baruch1@live.biu.ac.il>
2019-03-24 17:41:57 +0200

    mission container

--- ex1/MissionContainer.cs
+++ ex1/MissionContainer.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace Excercise_1
 {
-
+    public delegate double function(double val);
+    //constructor
     public class FunctionsContainer
     {
-
-
+        private Dictionary<string, function> dict;
+        public FunctionsContainer()
+        {
+            dict = new Dictionary<string, function>();
+        }
+        //indexer.in order to use [].
+        public function this[String str]
+        {
+            get
+            {
+                if (!dict.ContainsKey(str))
+                {
+                    dict.Add(str, val => val);//if the function is not in the map,return the value(do nothing).
+                }
+                return dict[str];
+            }
+            set
+            {
+                dict[str] = value;
+            }
+        }
+        public List<String> getAllMissions()
+        {
+            return dict.Keys.ToList();
+        }
     }
 }
